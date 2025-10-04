2b29e939b8b6a20a15ac67811be7dc93d07023e8
Njones27 <nathanjjones2004@gmail.com>
2025-10-04 02:55:21 -0400

    Initial IOS app setup

---
+++ WalkWorthy/WalkWorthy/WalkWorthyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkWorthyApp.swift
+//  WalkWorthy
+//
+//  Created by Nathan Jones on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkWorthyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
